1b9cf12bd6479c75d3bc7f1549a79a0f70f25b38
ritesh <riteshf7@gmail.com>
2025-07-12 16:37:38 +0530

    code cleanup and folder structure added

--- app/src/main/java/com/trex/simplesample/ui/MainActivity.kt
+++ app/src/main/java/com/trex/simplesample/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.trex.simplesample
+package com.trex.simplesample.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,28 +21,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        Text("Hello, World!")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimpleSampleTheme {
-        Greeting("Android")
     }
 }
